7d64e232cdf441a378c936d967472c21bbc09a20
Shadowfacts <me@shadowfacts.net>
2015-06-15 21:26:18 -0400

    Switch to using Slick for the carousel

--- static/js/nova.js
+++ static/js/nova.js
@@ -1,5 +1,34 @@
 // NOVA JavaScript
 // Author: shadowfacts
 $(document).ready(function() {
-	$('.carousel').carousel();
+	$('.nova-slider').slick({
+		centerMode: true,
+		centerPadding: '60px',
+		slidesToShow: 3,
+		autoplay: true,
+		autplaySpeed: 8000,
+		appendArrows: '.nova-slider-controls',
+		prevArrow: '<a href="#" class="left" role="button"><span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span><span class="sr-only">Previous</span></a>',
+		nextArrow: '<a href="#" class="right" role="button"><span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span><span class="sr-only">Next</span></a>',
+		responsive: [
+			{
+				breakpoint: 768,
+				settings: {
+					arrows: false,
+					centerMode: true,
+					centerPadding: '40px',
+					slidesToShow: 3
+				}
+			},
+			{
+				breakpoint: 480,
+				settings: {
+					arrows: false,
+					centerMode: true,
+					centerPadding: '40px',
+					slidesToShow: 3
+				}
+			}
+		]
+	});
 });
