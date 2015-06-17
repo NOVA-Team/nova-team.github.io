2df23c5c8e193a31241cad587dc9242e3340f088
Shadowfacts <me@shadowfacts.net>
2015-06-17 18:33:29 -0400

    Slow down carousel autoplay

--- static/js/nova.js
+++ static/js/nova.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
 		centerPadding: '60px',
 		slidesToShow: 3,
 		autoplay: true,
-		autplaySpeed: 8000,
+		autplaySpeed: 16000,
 		appendArrows: '.nova-slider-controls',
 		prevArrow: '<a href="javascript:void(0)" class="left" role="button"><span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span><span class="sr-only">Previous</span></a>',
 		nextArrow: '<a href="javascript:void(0)" class="right" role="button"><span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span><span class="sr-only">Next</span></a>',
